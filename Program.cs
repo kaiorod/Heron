4b5f2fd84c335b96d9bd90e2f6294cf80db7824d
kaiorod <caiorodrigo2775@gmail.com>
2021-09-12 22:12:15 -0300

    Finaliza Heron

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Heron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b ,c, p, area;
+
+            Console.Clear();
+            Console.Beep();
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("--- Área e Semiperímetro de um triângulo ---");
+            Console.ResetColor();
+
+            Console.Write("Lado 1 :");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Lado 2 :");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Lado 3 :");
+            c = double.Parse(Console.ReadLine());
+
+            p = (a + b + c) / 2;
+            area = Math.Sqrt(p*(p - a) * (p - b) * (p - c));
+
+            Console.Beep();
+            Console.Write($"\nSemiperímetro : ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(p);
+            Console.ResetColor();
+            Console.Write($"Área : ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(area);
+            Console.ResetColor();
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Heron
-Calcule a área de um triângulo qualquer, dadas as medidas dos 3 lados. Exiba o semiperímetro e a área.  Área (A):    Semiperímetro (p): metade da soma dos lados    Digite os lados do triângulo desejado.  Lado 1..: 3 Lado 2..: 25 Lado 3..: 26  Semiperímetro..: 27 Área...........: 36
+Calcule a área de um triângulo qualquer, dadas as medidas dos 3 lados. Exiba o semiperímetro e a área. 
+Área (A):   
+Semiperímetro (p): metade da soma dos lados 
+Digite os lados do triângulo desejado.  
+Lado 1..: 3 
+Lado 2..: 25
+Lado 3..: 26  
+Semiperímetro..: 27 
+Área...........: 36
